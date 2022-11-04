b436af46e20dfa093a33bf60721d823d1b390624
MatheusLevy <levybessa2001@hotmail.com>
2022-11-04 17:11:28 -0300

    refatorado código da Tela de Login

--- src/Telas/Login.java
+++ src/Telas/Login.java
@@ -9,91 +9,142 @@ import Controladores.ControladorDeAluno;
 import Controladores.ControladorDeBibliotecario;
 import Telas.Aluno.MenuAluno;
 import Telas.Bibliotecario.MenuBibliotecario;
+import Enum.TipoDePanel;
+import Enum.TipoUsuarios;
 
 public class Login extends JFrame implements ActionListener {
     JTextField tfEmail, tfSenha;
-    JComboBox cbUsuario;
+    JComboBox<String> cboTipoUsuario;
     JLabel message;
+
+    private FlowLayout layoutPadrao(){
+        return new FlowLayout(FlowLayout.CENTER, 20, 20);
+    }
+
+    private JLabel criarLabel(String text){
+        JLabel label = new JLabel();
+        label.setText(text);
+        return label;
+    }
+
+    private JTextField criarTextField(String text){
+        JTextField textField = new JTextField();
+        textField.setText(text);
+        textField.setColumns(25);
+        return textField;
+    }
+
+    private JPanel tipoUsuarioPanel(String[] tipos){
+        JLabel lblTipoUsuario = criarLabel("Tipo Usuário:");
+        this.cboTipoUsuario = new JComboBox<>(tipos);
+        JPanel tipoUsuarioPanel = new JPanel();
+        tipoUsuarioPanel.add(lblTipoUsuario);
+        tipoUsuarioPanel.add(cboTipoUsuario);
+        return tipoUsuarioPanel;
+    }
+
+    private Boolean isEmail(TipoDePanel tipoDePanel){
+        return tipoDePanel == TipoDePanel.Email;
+    }
+
+    private JTextField getLocalTextField(TipoDePanel tipo){
+        if (isEmail(tipo))
+           return this.tfEmail;
+        else
+            return this.tfSenha;
+    }
+
+    private void initLocalTextField(TipoDePanel tipoDePanel){
+        if (isEmail(tipoDePanel))
+            this.tfEmail = criarTextField("");
+        else
+            this.tfSenha = criarTextField("");
+    }
+
+    private JPanel labelETextFieldPanel(String labelText, TipoDePanel tipo){
+        JLabel label = criarLabel(labelText);
+        JPanel panel = new JPanel(layoutPadrao());
+        initLocalTextField(tipo);
+        panel.add(label);
+        panel.add(getLocalTextField(tipo));
+        return panel;
+    }
+
+    private JPanel botaoPanel(String buttonText){
+        JPanel botaoPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        JButton botao = new JButton(buttonText);
+        botao.addActionListener(this);
+        this.message = criarLabel("");
+        botaoPanel.add(message);
+        botaoPanel.add(botao);
+        return botaoPanel;
+    }
+
     public Login() {
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 400, 10));
-
-        //Tipo usuario
-        String[] patterns = {
+        String[] tiposUsuario = {
                 "Bibliotecario",
                 "Aluno",
         };
-        JLabel lbUsuario = new JLabel();
-
-        cbUsuario = new JComboBox(patterns);
-        JPanel pnUsuario = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 20));
-        lbUsuario.setText("Tipo de Usuario :");
-        pnUsuario.add(lbUsuario);
-        pnUsuario.add(cbUsuario);
-        panel.add(pnUsuario);
-
-        // Email Label
-        JPanel pnEmail = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 20));
-        JLabel lbEmail = new JLabel();
-        tfEmail = new JTextField();
-        lbEmail.setText("Email :");
-        tfEmail.setColumns(25);
-        pnEmail.add(lbEmail);
-        pnEmail.add(tfEmail);
-        panel.add(pnEmail);
-
-        // Password
-        JPanel pnSenha = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 20));
-        JLabel lbSenha = new JLabel();
-        lbSenha.setText("Senha :");
-        tfSenha = new JTextField();
-        tfSenha.setColumns(25);
-        pnSenha.add(lbSenha);
-        pnSenha.add(tfSenha);
-        panel.add(pnSenha);
-
-        // Submit
-        JPanel pnEnviar = new JPanel(new FlowLayout(FlowLayout.CENTER, 00, 0));
-        JButton enviar = new JButton("Entrar");
-        message = new JLabel();
-        enviar.addActionListener(this);
-        pnEnviar.add(enviar);
-        pnEnviar.add(message);
-        panel.add(pnEnviar);
-
-        // Adding the listeners to components..
+        panel.add(tipoUsuarioPanel(tiposUsuario));
+        panel.add(labelETextFieldPanel("Email:", TipoDePanel.Email));
+        panel.add(labelETextFieldPanel("Senha:", TipoDePanel.Senha));
+        panel.add(botaoPanel("Entrar"));
         add(panel, BorderLayout.CENTER);
         setTitle("Login ");
     }
 
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        boolean res = false;
-        if( cbUsuario.getSelectedIndex() == 0 ){
-            res = ControladorDeBibliotecario.FazerLogin(tfEmail.getText(), tfSenha.getText());
-        }else if( cbUsuario.getSelectedIndex() == 1 ){
-            res = ControladorDeAluno.FazerLogin(tfEmail.getText(), tfSenha.getText());
-        }
 
-        if( res ){
-            this.dispose();
-            if( cbUsuario.getSelectedIndex() == 0 ){
-                MenuBibliotecario app = new MenuBibliotecario();
-                app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                app.setPreferredSize(app.getSize());
-                app.setSize(800, 600);
-                app.setVisible(true);
-
-            } else if( cbUsuario.getSelectedIndex() == 1 ){
-                MenuAluno app = new MenuAluno();
-                app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                app.setPreferredSize(app.getSize());
-                app.setSize(800, 600);
-                app.setVisible(true);
-            }
-         }else{
-             message.setText("Usuario ou senha Invalidos");
-         }
+    private Boolean selecionouAluno(){
+        System.out.println(cboTipoUsuario.getSelectedIndex());
+        return cboTipoUsuario.getSelectedIndex() == 1;
 
     }
 
+    private Boolean selecionouBibliotecario(){
+        return  cboTipoUsuario.getSelectedIndex() == 1;
+    }
+
+    private Boolean autenticar(){
+        if(selecionouAluno()){
+            return ControladorDeAluno.FazerLogin(tfEmail.getText(), tfSenha.getText());
+        }else if(selecionouBibliotecario()){
+            return ControladorDeBibliotecario.FazerLogin(tfEmail.getText(), tfSenha.getText());
+        }
+        return false;
+    }
+
+    private JFrame autenticadoPage(TipoUsuarios tipoUsuario){
+        if(tipoUsuario == TipoUsuarios.Aluno)
+            return new MenuAluno();
+        else if (tipoUsuario == TipoUsuarios.Bibliotecario)
+            return new MenuBibliotecario();
+        return null;
+    }
+
+    private void abrirMenuFrame(TipoUsuarios tipoUsuarios){
+        JFrame app = autenticadoPage(tipoUsuarios);
+        assert app != null;
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setPreferredSize(app.getSize());
+        app.setSize(800, 600);
+        app.setVisible(true);
+    }
+
+    private TipoUsuarios getTipoUsuario(){
+        if(selecionouAluno())
+            return TipoUsuarios.Aluno;
+        else
+            return TipoUsuarios.Bibliotecario;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        boolean autenticado = autenticar();
+        if(autenticado){
+            abrirMenuFrame(getTipoUsuario());
+         }else{
+             message.setText("Usuario ou senha Invalidos");
+         }
+    }
 }
